c794c991078b76ad7f9eccb4637c852c8fe36320
NotEles <1589921216@qq.com>
2025-02-24 21:43:32 +0800

    nes starting

---
+++ assignment1/calculator/CalcuOnWin/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcuOnWin
+{
+    public partial class Form1: Form
+    {
+        int add1, add2, sum;
+        string tem;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ assignment1/calculator/calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace cal
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            int sum, ad1, ad2;
+            string tem;
+            tem = Console.ReadLine();
+            ad1 = Int32.Parse(tem);
+            tem = Console.ReadLine();
+            ad2 = Int32.Parse(tem);
+            sum = ad1 + ad2;
+            Console.WriteLine("{0}+{1}={2}", ad1, ad2, sum);
+        }
+    }
+}
