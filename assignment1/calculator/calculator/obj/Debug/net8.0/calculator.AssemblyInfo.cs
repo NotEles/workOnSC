f9fe8acd7c2a67781451993a46c56439d5b49f7b
NotEles <1589921216@qq.com>
2025-02-27 12:13:15 +0800

    adding assignment2

--- assignment1/calculator/calculator/obj/Debug/net8.0/calculator.AssemblyInfo.cs
+++ assignment1/calculator/calculator/obj/Debug/net8.0/calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402942789e9b5f033a005701c8ea9ee6ce4f4412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc906be2005d090c48e8e9ca4036ebd737057e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
